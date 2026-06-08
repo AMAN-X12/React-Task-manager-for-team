19a5cf89e71fc2e93a85891b8f8d9cab09993548
AMAN-X12 <197904711+AMAN-X12@users.noreply.github.com>
2026-06-09 00:20:43 +0500

    fix: update CORS to allow production frontend

--- backend/server.js
+++ backend/server.js
@@ -27,8 +27,6 @@ app.use(cors({
   },
   credentials: true
 }));
-// ----------------------------------
-
 app.set('trust proxy', 1);
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
